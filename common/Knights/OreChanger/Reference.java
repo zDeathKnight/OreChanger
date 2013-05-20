5da480de30cf71fbb504652e5af955ab087a93be
mark cooper <markcoo36@gmail.com>
2013-05-20 23:26:04 +0100

    Revert "test"
    This reverts commit 1ca3de91e667e004aab1db179a6c656794b60bc6.

--- common/Knights/OreChanger/Reference.java
+++
@@ -1,9 +0,0 @@
-package Knights.OreChanger;
-
-public class Reference {
-	
-	public static final String MOD_ID = "OreChanger";
-	public static final String MOD_NAME = "OreChanger";
-	public static final String VERSION = "2.6";
-
-}
